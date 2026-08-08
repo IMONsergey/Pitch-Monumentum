9f1d87fbdc636d0cb9fdca338bd72af60ae1622c
IMONsergey <jabba.person@gmail.com>
2026-08-08 10:49:29 +0300

    feat: install Pro Editor UI in editor runtime

--- apps/editor-spike/src/bootstrap.ts
+++ apps/editor-spike/src/bootstrap.ts
@@ -1,6 +1,8 @@
 import { installPitchRichTextUI } from "./rich-text-ui.js";
 import { installPitchAutoLayoutUI } from "./auto-layout-ui.js";
+import { installPitchProEditorUI } from "./pro-editor-ui.js";
 import "./client.js";
 
 installPitchRichTextUI();
 installPitchAutoLayoutUI();
+installPitchProEditorUI();
