90cd3a4fd61527a0e4fcb651f4b439c446283034
IMONsergey <jabba.person@gmail.com>
2026-08-08 11:30:18 +0300

    feat: install Assets AI Image and Export Center UI

--- apps/editor-spike/src/bootstrap.ts
+++ apps/editor-spike/src/bootstrap.ts
@@ -1,6 +1,10 @@
 import { installPitchRichTextUI } from "./rich-text-ui.js";
 import { installPitchAutoLayoutUI } from "./auto-layout-ui.js";
+import { installPitchAssetsAiUI } from "./assets-ai-ui.js";
+import { installPitchExportCenterUI } from "./export-center-ui.js";
 import "./client.js";
 
 installPitchRichTextUI();
 installPitchAutoLayoutUI();
+installPitchAssetsAiUI();
+installPitchExportCenterUI();
