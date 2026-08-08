bc9199357b679279286a90317326b182ca784c38
IMONsergey <jabba.person@gmail.com>
2026-08-08 11:31:36 +0300

    feat: install original image renderer on editor canvas

--- apps/editor-spike/src/bootstrap.ts
+++ apps/editor-spike/src/bootstrap.ts
@@ -2,9 +2,11 @@ import { installPitchRichTextUI } from "./rich-text-ui.js";
 import { installPitchAutoLayoutUI } from "./auto-layout-ui.js";
 import { installPitchAssetsAiUI } from "./assets-ai-ui.js";
 import { installPitchExportCenterUI } from "./export-center-ui.js";
+import { installPitchImageRenderer } from "./image-render-ui.js";
 import "./client.js";
 
 installPitchRichTextUI();
 installPitchAutoLayoutUI();
 installPitchAssetsAiUI();
 installPitchExportCenterUI();
+installPitchImageRenderer();
