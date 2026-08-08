fd7d7cdac1541fe6914becb260016d29691799fa
IMONsergey <jabba.person@gmail.com>
2026-08-08 07:54:12 +0300

    feat: enable rich text and auto-layout editor modes

--- apps/editor-spike/src/bootstrap.ts
+++ apps/editor-spike/src/bootstrap.ts
@@ -1,4 +1,6 @@
 import { installPitchRichTextUI } from "./rich-text-ui.js";
+import { installPitchAutoLayoutUI } from "./auto-layout-ui.js";
 import "./client.js";
 
 installPitchRichTextUI();
+installPitchAutoLayoutUI();
