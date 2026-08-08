615ccfa78e0953359b99f0085f00bb15a33fe1bf
IMONsergey <jabba.person@gmail.com>
2026-08-08 10:58:25 +0300

    feat: install live branch-safe history shortcuts

--- apps/editor-spike/src/bootstrap.ts
+++ apps/editor-spike/src/bootstrap.ts
@@ -1,8 +1,10 @@
 import { installPitchRichTextUI } from "./rich-text-ui.js";
 import { installPitchAutoLayoutUI } from "./auto-layout-ui.js";
 import { installPitchProEditorUI } from "./pro-editor-ui.js";
+import { installPitchHistoryShortcuts } from "./history-shortcuts.js";
 import "./client.js";
 
 installPitchRichTextUI();
 installPitchAutoLayoutUI();
 installPitchProEditorUI();
+installPitchHistoryShortcuts();
