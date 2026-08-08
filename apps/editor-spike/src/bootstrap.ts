ce823ffe18c9a8249d62787a2163595341f7ef8b
IMONsergey <jabba.person@gmail.com>
2026-08-08 11:09:50 +0300

    feat: install precise Inspector in Pro Editor

--- apps/editor-spike/src/bootstrap.ts
+++ apps/editor-spike/src/bootstrap.ts
@@ -2,9 +2,11 @@ import { installPitchRichTextUI } from "./rich-text-ui.js";
 import { installPitchAutoLayoutUI } from "./auto-layout-ui.js";
 import { installPitchProEditorUI } from "./pro-editor-ui.js";
 import { installPitchHistoryShortcuts } from "./history-shortcuts.js";
+import { installPitchInspectorUI } from "./inspector-ui.js";
 import "./client.js";
 
 installPitchRichTextUI();
 installPitchAutoLayoutUI();
 installPitchProEditorUI();
 installPitchHistoryShortcuts();
+installPitchInspectorUI();
